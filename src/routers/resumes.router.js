5770a54a56fa7927c25e953e47704b17a5378276
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-05-27 18:42:21 +0900

    resume read complete

--- src/routers/resumes.router.js
+++ src/routers/resumes.router.js
@@ -1,4 +1,4 @@
-import express, { application } from 'express';
+import express from 'express';
 
 import { prisma } from '../utils/prisma.util.js';
 import { verifyAccessToken } from '../middlewares/require-access-token.middleware.js';
@@ -7,39 +7,82 @@ import { resumePostValidation } from '../middlewares/joi-handler.middleware.js';
 const router = express.Router();
 
 router.post('/post', verifyAccessToken, async (req, res, next) => {
-  try {
-    const resumePostVal = await resumePostValidation.validateAsync(req.body);
-    const userId = req.userId;
-    const { title, introduce } = resumePostVal;
+    try {
+        const resumePostVal = await resumePostValidation.validateAsync(req.body);
+        const UserId = req.userId;
+        const { title, introduce } = resumePostVal;
 
-    const resumeCreate = await prisma.resume.create({
-      data: {
-        UserId: userId,
-        title,
-        introduce,
-      },
-    });
+        if (!UserId) {
+            return res.status(400).json({ error: 'User ID not provided' });
+        }
 
-    return res.status(201).json({
-      id: resumeCreate.UserId,
-      resumeId: resumeCreate.resumeId,
-      title: resumeCreate.title,
-      introduce: resumeCreate.introduce,
-      applicationStatus: resumeCreate.applicationStatus,
-      createdAt: resumeCreate.createdAt,
-      updatedAt: resumeCreate.updatedAt,
-    });
-  } catch (error) {
-    next(error);
-  }
+        const resumeCreate = await prisma.resume.create({
+            data: {
+                UserId: UserId,
+                title,
+                introduce
+            }
+        });
+
+        return res.status(201).json({
+            UserId: resumeCreate.UserId,
+            resumeId: resumeCreate.resumeId,
+            title: resumeCreate.title,
+            introduce: resumeCreate.introduce,
+            applicationStatus: resumeCreate.applicationStatus,
+            createdAt: resumeCreate.createdAt,
+            updatedAt: resumeCreate.updatedAt,
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.get('/get', verifyAccessToken, async (req, res, next) => {
-  try {
-    
-  } catch (error) {
-    next(error);
-  }
+router.get('/', verifyAccessToken, async (req, res, next) => {
+    try {
+        const UserId = req.userId;
+
+        if (!UserId) {
+            return res.status(400).json({ error: 'User ID not provided' });
+        }
+
+        let sortDirection = 'DESC';
+        const { sort } = req.query;
+        if (sort && sort.toLowerCase() === 'asc') {
+            sortDirection = 'ASC';
+        };
+
+        const resumeSearch = await prisma.resume.findMany({
+            where: { UserId: UserId },
+            orderBy: { createdAt: sortDirection },
+            select: {
+                resumeId: true,
+                title: true,
+                introduce: true,
+                applicationStatus: true,
+                createdAt: true,
+                updatedAt: true,
+                Users: { select: { name: true } }
+            }
+        });
+
+        if (resumeSearch.length === 0) {
+            return res.status(200).json([]);
+        };
+
+        return res.status(200).json({ resumeSearch });
+
+    } catch (error) {
+        next(error);
+    }
 });
+
+router.get('/?userId', verifyAccessToken, async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+})
 
 export { router };
